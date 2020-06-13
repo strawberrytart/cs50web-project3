28b2e5f24870867ff2811229b35c5d2e1585d2a4
strawberrytart <jasonleeweihan@gmail.com>
2020-06-13 18:59:44 +0800

    edit wsgi

--- pizza/wsgi.py
+++ pizza/wsgi.py
@@ -15,3 +15,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizza.settings")
 
 application = get_wsgi_application()
+SECRET_KEY = os.environ["i0&iq&e9u9h6(4_7%pt2s9)f=c$kso=k$c$w@fi9215s=1q0^d"]
